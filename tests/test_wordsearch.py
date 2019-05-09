d1448c8647322fae4a84482bc00f591635fdd7e0
topher <topher@findtopher.com>
2019-05-09 11:07:46 -0400

    refactor to provide a Matrix class and an executable to play the game

--- tests/test_wordsearch.py
+++ tests/test_wordsearch.py
@@ -3,22 +3,18 @@ Test the wordsearch module
 """
 #pylint: disable=redefined-outer-name
 import pytest
-from wordsearch.wordsearch import WordSearch
+from wordsearch.wordsearch import Matrix
 
 
 @pytest.fixture
 def testable_wordsearch():
     """Create a test matrix to be used in future tests"""
 
-    word_list = ['YUM', 'BOA', 'YEP', 'YAM', 'CBM', 'GUY', 'MOM', 'UB', 'DAD']
-
-    wordsearch = WordSearch(3, word_list)
+    wordsearch = Matrix(3)
 
     wordsearch.matrix[0] = ['Y', 'U', 'M']
     wordsearch.matrix[1] = ['A', 'O', 'B']
     wordsearch.matrix[2] = ['M', 'F', 'C']
-
-    wordsearch.words_found = wordsearch.find_words()
 
     return wordsearch
 
@@ -26,8 +22,7 @@ def test_new_matrix():
     """Test creating a new matrix and confirm there are no default values remaining"""
 
     zeros_exist = False
-    word_list = ['SOME', 'SMURFY', 'LIST']
-    wordsearch = WordSearch(5, word_list)
+    wordsearch = Matrix(5)
 
     for _, row in enumerate(wordsearch.matrix):
         if '0' in row:
@@ -39,30 +34,29 @@ def test_new_matrix():
 def test_finding_words_in_row(testable_wordsearch):
     """Test finding words in a row of a given matrix"""
 
-    assert 'YUM' in testable_wordsearch.words_found
-    assert 'BOA' in testable_wordsearch.words_found
-    assert 'YEP' not in testable_wordsearch.words_found
+    assert 'YUM' in testable_wordsearch
+    assert 'BOA' in testable_wordsearch
+    assert 'YEP' not in testable_wordsearch
 
 def test_finding_words_in_column(testable_wordsearch):
     """Test finding words in a column of a given matrix"""
 
-    assert 'YAM' in testable_wordsearch.words_found
-    assert 'CBM' in testable_wordsearch.words_found
-    assert 'GUY' not in testable_wordsearch.words_found
+    assert 'YAM' in testable_wordsearch
+    assert 'CBM' in testable_wordsearch
+    assert 'GUY' not in testable_wordsearch
 
 def test_finding_words_in_diagonals(testable_wordsearch):
     """Test finding words in the diagonals of a given matrix"""
 
-    assert 'MOM' in testable_wordsearch.words_found
-    assert 'UB' in testable_wordsearch.words_found
-    assert 'DAD' not in testable_wordsearch.words_found
+    assert 'MOM' in testable_wordsearch
+    assert 'UB' in testable_wordsearch
+    assert 'DAD' not in testable_wordsearch
 
 def test_printing_wordsearch(testable_wordsearch, capsys):
     """Test printing the WordSearch object"""
 
-    testable_wordsearch.words_found = ['DAD', 'BROTHER', 'ZEBRA']
     print(testable_wordsearch)
     captured = capsys.readouterr()
 
-    assert captured.out == "\nY U M\nA O B\nM F C\n\nBROTHER DAD ZEBRA\n"
+    assert captured.out == "Y U M\nA O B\nM F C\n"
     assert captured.err == ''
--- wordsearch/wordsearch.py
+++ wordsearch/wordsearch.py
@@ -1,32 +1,31 @@
 """
 Create a random wordsearch grid and identify words included
 """
-import argparse
 import random
 import string
 import numpy as np
 
-class WordSearch:
+class Matrix:
     """Create a matrix of characters and find all the words included"""
 
-    def __init__(self, matrix_size, words_to_find):
+    def __init__(self, matrix_size):
         self.matrix_size = matrix_size
         self.matrix = self.new_matrix()
-        self.words_to_find = words_to_find
-        self.words_found = self.find_words()
 
     def __repr__(self):
-        matrix_strings = []
-        self_string = "\n"
+        matrix_rows = []
 
         for _, row in enumerate(self.matrix):
-            matrix_strings.append(" ".join(row))
+            matrix_rows.append(" ".join(row))
 
-        self_string += "\n".join(matrix_strings)
-        self_string += "\n\n"
-        self_string += " ".join(sorted(self.words_found))
+        return "\n".join(matrix_rows)
 
-        return self_string
+    def __contains__(self, word):
+
+        if self._find_word_in_row(word) or self._find_word_in_column(word) or self._find_word_in_diagonal(word):
+            return True
+
+        return False
 
     def new_matrix(self):
         """Return a matrix of uppercase characters of height and width self.matrix_size"""
@@ -44,99 +43,56 @@ class WordSearch:
 
         return wordsearch_matrix
 
-    def find_words(self):
-        """Return a list of words found in the matrix"""
+    def _find_word_in_row(self, word):
+        """Return True if the word exists in the rows of the matrix, False if it does not"""
 
-        found_words = set()
-        found_words.update(self._find_words_in_row())
-        found_words.update(self._find_words_in_column())
-        found_words.update(self._find_words_in_diagonal())
-
-        return found_words
-
-    def _find_words_in_row(self):
-        """Return a set of words found in a row of the matrix"""
-
-        words_found = set()
         for _, row in enumerate(self.matrix):
             search_string = "".join(row)
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
-        return words_found
+        return False
 
-    def _find_words_in_column(self):
-        """Return a set of words found in a column of the matrix"""
+    def _find_word_in_column(self, word):
+        """Return True if the word exists in the columns of the matrix, False if it does not"""
 
-        words_found = set()
         for col in range(self.matrix_size):
             search_string = "".join(self.matrix[:, col])
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
-        return words_found
+        return False
 
-    def _find_words_in_diagonal(self):
-        """Return a set of words found in the diagonals of the matrix"""
+    def _find_word_in_diagonal(self, word):
+        """Return True if the word exists in the diagonals of the matrix, False if it does not"""
 
-        words_found = set()
         for diag in range(self.matrix_size):
             search_string = "".join(np.diagonal(self.matrix, diag))
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
         for diag in range(-1, 0-self.matrix_size, -1):
             search_string = "".join(np.diagonal(self.matrix, diag))
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
         flip_matrix = np.fliplr(self.matrix)
         for diag in range(self.matrix_size):
             search_string = "".join(np.diagonal(flip_matrix, diag))
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
         for diag in range(-1, 0-self.matrix_size, -1):
             search_string = "".join(np.diagonal(flip_matrix, diag))
-            words_found.update(self._find_words_in_string(search_string))
+            if self._find_word_in_string(search_string, word):
+                return True
 
-        return words_found
+        return False
 
-    def _find_words_in_string(self, search_string):
-        """Return a list of words found in a given string"""
+    def _find_word_in_string(self, search_string, word):
+        """Return True if the word exists in the provided string"""
 
-        words_found = []
+        if (word in search_string) or (word in search_string[::-1]):
+            return True
 
-        for word in self.words_to_find:
-            if (word in search_string) or (word in search_string[::-1]):
-                words_found.append(word)
-
-        return words_found
-
-def grid_size_type(size):
-    """Create a new argparse type to validate grid size is an integer greater than 2"""
-
-    size = int(size)
-    if size <= 2:
-        raise argparse.ArgumentTypeError("Minimum grid size is 3 x 3")
-    return size
-
-def create_word_list_from_file(filehandle, grid_size):
-    """Return a list of words to search for"""
-
-    word_list = set(line.strip().upper() for line in filehandle if len(line) <= grid_size)
-
-    return word_list
-
-def main():
-    """Create a wordsearch with provided size and word list file"""
-
-    parser = argparse.ArgumentParser(description='Create a new Wordsearch')
-    parser.add_argument('size', type=grid_size_type,
-                        help="height and width of our wordsearch grid (min: 3)")
-    parser.add_argument('wordfile', type=argparse.FileType('r'),
-                        help="file including words to search for")
-    parser_args = parser.parse_args()
-
-    words_to_find = create_word_list_from_file(parser_args.wordfile, parser_args.size)
-    parser_args.wordfile.close()
-    new_wordsearch = WordSearch(parser_args.size, words_to_find)
-    print(new_wordsearch)
-
-if __name__ == "__main__":
-    main()
+        return False
